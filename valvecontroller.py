799bb0dc171867b516afcbe1cfb373e19c790e89
iamniklas <niklas.englmeier@web.de>
2021-06-27 13:03:07 +0200

    Auto Watering Duration added

--- valvecontroller.py
+++ valvecontroller.py
@@ -15,6 +15,8 @@ avgMoisture = 1000
 avgWateringMinimum = 300
 useSensors = 6
 
+autoWateringDuration = 3
+
 GPIO.setmode(GPIO.BCM)
 
 GPIO.setup(errorLedPin, GPIO.OUT)
@@ -28,7 +30,7 @@ def water(duration):
 	time.sleep(duration)
 	GPIO.output(relayPin0, GPIO.HIGH)
 	GPIO.output(wateringLedPin, GPIO.LOW)
-	time.sleep(2)
+	time.sleep(5)
 
 while True:
 	try:
@@ -52,6 +54,7 @@ while True:
 		sensor = sensorrequest.read()
 		sensorData = json.loads(sensor)
 		print(len(sensorData))
+		#Calculate the sensor average value
 		sensorTotal = 0
 		for sensor in range(useSensors):
 			sensorTotal = sensorTotal + sensorData[sensor]["value"]
@@ -59,7 +62,11 @@ while True:
 		print(str(sensorTotal / useSensors))
 		avgMoisture = int(sensorTotal / useSensors)
 
+		#If the schedule_watering bool is set to true or the average moisture (calculated right before) is lower than the sensor watering trigger value
 		if(data[0]['schedule_watering'] or avgMoisture < avgWateringMinimum):
+			if(data[0]['schedule_watering_duration'] == 0):
+				data[0]['schedule_watering_duration'] = autoWateringDuration
+
 			water(int(data[0]['schedule_watering_duration']))
 			connBody = "{\"id\": 1, \"last_time_watered\": " +str(int(datetime.datetime.now().timestamp()))+ " , \"schedule_watering\": false, \"schedule_watering_duration\": 0.0}"
 			postreq = requests.post("http://000raspberry.ddns.net/cultivates/api/valve?password=12345678", connBody)
